ab0920739c1815d4f487dfdf16a5eabe19d712c5
Byron <53088136+byronbytes@users.noreply.github.com>
2023-05-15 14:18:52 -0400

    Update badges.js

--- badges.js
+++ badges.js
@@ -1,8 +1,11 @@
 const Badges = {
 	FOUNDERS: "Founder", // The founders of Fibr
-	STAFF: "Staff", // Staff members who help moderate Fibr as a whole
+	STAFF: "Staff", // Staff members who help moderate Fibr as a whole (https://github.com/Fibr-Chat/Fibr/blob/master/assets/Staffv1.png)
 	BETA_TESTER: "Beta Tester", // People who beta tested Fibr in early development
-  SUPPORTER: "Supporter", // People who supported by donating to Fibr
+  	SUPPORTER: "Supporter", // People who supported by donating to Fibr (https://github.com/Fibr-Chat/Fibr/blob/master/assets/Staffv1.png)
+	VERIFIED: "Verified", // People who verify their account with an email, or something.
 	DEFAULT: " " // No custom role
 }
+
+// Add a boolean to hide staff badge or something
 // Do whatever you want with this, it's going to be similar to discord's badge system but less pay2win if you know what I mean.
