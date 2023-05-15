7c52d117e2cf9ddfa30c0d3a449d7c05c9cca127
Byron <53088136+byronbytes@users.noreply.github.com>
2023-05-15 13:54:45 -0400

    Create badges.js

---
+++ badges.js
@@ -0,0 +1,8 @@
+const Badges = {
+	FOUNDERS: "Founder", // The founders of Fibr
+	STAFF: "Staff", // Staff members who help moderate Fibr as a whole
+	BETA_TESTER: "Beta Tester", // People who beta tested Fibr in early development
+  SUPPORTER: "Supporter", // People who supported by donating to Fibr
+	DEFAULT: " " // No custom role
+}
+// Do whatever you want with this, it's going to be similar to discord's badge system but less pay2win if you know what I mean.
